d71ef0134915d9b88c09eb4e90f5d4cfbe8a02e9
Tech_Solution <onpointdev0@gmail.com>
2024-08-10 12:25:07 +0300

    Implement the server in the main method

--- server.c
+++ server.c
@@ -52,6 +52,34 @@ int main(int argc, char *argv[]) {
     int opt;
     char password_file[MAX_FILE_PATH_SIZE] = "";
 
+    int server_socket, client_socket;
+struct sockaddr_in server_address, client_address;
+socklen_t client_address_len = sizeof(client_address);
+
+server_socket = socket(AF_INET, SOCK_STREAM, 0);
+if (server_socket < 0) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+}
+
+server_address.sin_family = AF_INET;
+server_address.sin_addr.s_addr = INADDR_ANY;
+server_address.sin_port = htons(server_port);
+
+if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+    perror("Bind failed");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+}
+
+if (listen(server_socket, 5) < 0) {
+    perror("Listen failed");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+}
+
+printf("Server listening on port %d...\n", server_port);
+
     while ((opt = getopt(argc, argv, "d:p:u:")) != -1) {
         switch (opt) {
             case 'd':
